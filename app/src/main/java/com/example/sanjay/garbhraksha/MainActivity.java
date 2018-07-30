b7d37a40e703d9b5611df787108f24218c7767d8
sanjaynath <nsanjay725@gmail.com>
2018-07-30 16:34:44 +0530

    Added Splash screen

--- app/src/main/java/com/example/sanjay/garbhraksha/MainActivity.java
+++ app/src/main/java/com/example/sanjay/garbhraksha/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.sanjay.garbhraksha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //members
+
+    private ImageView background;
+    private Button anganSignIn;
+    private Button motherSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       init();
+
+    }
+    void init(){
+        background = (ImageView) findViewById(R.id.background);
+        anganSignIn = (Button) findViewById(R.id.anganwadisignin);
+        motherSignIn = (Button) findViewById(R.id.mothersignin);
     }
 }
